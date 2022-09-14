3059bcb37c7a04b954bc3caf3e09c15760e807db
Rahwa <rahwafesseha@gmail.com>
2022-09-14 16:05:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Milen Fiseha</td>
+            <td>19 bywater, London</td>
+            <td>0789576856</td>
+            <td>milen.ha2gmail.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
